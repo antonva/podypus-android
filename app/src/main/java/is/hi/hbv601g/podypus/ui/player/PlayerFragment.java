e5aa1cc92b1d40e857439988fcbca81ad029420a
Matthias Dan Flemmingsson <matthiasdanflemmingsson@Matthiass-MacBook-Pro.local>
2020-03-05 23:41:59 +0000

    Added icons for the player

--- app/src/main/java/is/hi/hbv601g/podypus/ui/player/PlayerFragment.java
+++ app/src/main/java/is/hi/hbv601g/podypus/ui/player/PlayerFragment.java
@@ -1,4 +1,4 @@
-package is.hi.hbv601g.podypus.ui.notifications;
+package is.hi.hbv601g.podypus.ui.player;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import is.hi.hbv601g.podypus.R;
 
-public class NotificationsFragment extends Fragment {
+public class PlayerFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private PlayerViewModel playerViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        playerViewModel =
+                ViewModelProviders.of(this).get(PlayerViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(this, new Observer<String>() {
+        playerViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
@@ -32,4 +32,6 @@ public class NotificationsFragment extends Fragment {
         });
         return root;
     }
+
+
 }
--- app/src/main/java/is/hi/hbv601g/podypus/ui/player/PlayerViewModel.java
+++ app/src/main/java/is/hi/hbv601g/podypus/ui/player/PlayerViewModel.java
@@ -1,16 +1,16 @@
-package is.hi.hbv601g.podypus.ui.notifications;
+package is.hi.hbv601g.podypus.ui.player;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class PlayerViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public PlayerViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is the player");
     }
 
     public LiveData<String> getText() {
