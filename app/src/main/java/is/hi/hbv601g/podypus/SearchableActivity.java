5d7089594087ba50c7e1fbc379a32d01ac15ef26
YlfaHa <ylh1@hi.is>
2020-03-31 16:25:58 +0000

    start implementing searchChannel method

--- app/src/main/java/is/hi/hbv601g/podypus/SearchableActivity.java
+++ app/src/main/java/is/hi/hbv601g/podypus/SearchableActivity.java
@@ -2,10 +2,10 @@ package is.hi.hbv601g.podypus;
 
 import android.app.Activity;
 import android.app.SearchManager;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.widget.ArrayAdapter;
@@ -13,8 +13,14 @@ import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 
@@ -23,14 +29,12 @@ import is.hi.hbv601g.podypus.R;
 //Start of search activity implementation - possibly
 public class SearchableActivity extends Activity {
 
-    //private static final String TAG = "SearchActivity";
+    private String TAG = SearchableActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_search);
-
-
 
         // Get the intent, verify the action and get the query
         Intent intent = getIntent();
@@ -41,6 +45,39 @@ public class SearchableActivity extends Activity {
     }
     //TODO: implement method that conducts search for podcast
     private void searchChannels(String query) {
+        String url = "https://podypus.punk.is/";
 
+        //Volley json array request object
+        JsonArrayRequest request = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+
+                        if (response.length() > 0) {
+                            //looping through json and adding to podcast list
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    JSONObject podcastObj = response.getJSONObject(i);
+                                    String title = podcastObj.getString("title");
+                                    String imageURL = podcastObj.getString("imageURL");
+                                    //búa til podcast hlut og setja í lista??
+                                    //Channel c = new Channel()
+                                    //channelList.add();
+                                } catch (JSONException e) {
+                                    Log.e(TAG, "JSON Parsing error: " + e.getMessage());
+                                }
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Server Error: " + error.getMessage());
+
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
