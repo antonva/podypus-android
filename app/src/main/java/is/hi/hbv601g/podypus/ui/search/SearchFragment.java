f9d8742ee6d52542846f3cd2cded26496eaeceb8
YlfaHa <ylh1@hi.is>
2020-04-08 10:19:55 +0000

    tidy up dead code and start implementing search in fragment

--- app/src/main/java/is/hi/hbv601g/podypus/ui/search/SearchFragment.java
+++ app/src/main/java/is/hi/hbv601g/podypus/ui/search/SearchFragment.java
@@ -1,25 +1,27 @@
 package is.hi.hbv601g.podypus.ui.search;
 
-import android.app.SearchManager;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 import is.hi.hbv601g.podypus.R;
 
@@ -47,6 +49,8 @@ public class SearchFragment extends Fragment {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 Log.println(Log.INFO,"BOOP", "query: " + query);
+                searchChannels(query);
+                searchView.setQuery("", false);
                 return false;
             }
 
@@ -55,8 +59,9 @@ public class SearchFragment extends Fragment {
                 return false;
             }
         });
-        /*final ImageView closeButton = searchView.findViewById(R.id.search_close_btn);
-        closeButton.setOnClickListener(new View.OnClickListener() {
+        //TODO: get clear button on searchView to work (x-button) - currently crashes app
+        /*final ImageView closeButton = root.findViewById(R.id.search_close_btn);
+        closeButton.setOnClickListener(new SearchView.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -64,7 +69,63 @@ public class SearchFragment extends Fragment {
             }
         });*/
 
+
     return root;
     }
+    //TODO: implement method that conducts search for podcast
+    private void searchChannels(String query) {
+        Log.println(Log.INFO,"BOOP", "searchChannel");
+        String url = "https://podypus.punk.is/search";
 
+        //Volley json array request object
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.println(Log.INFO,"SEARCH", "Response: " + response.toString());
+                        //textView.setText("Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("ERROR", "Server Error: " + error.getMessage());
+                        Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        //MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+
+        /*JsonArrayRequest request = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+
+                        if (response.length() > 0) {
+                            //looping through json and adding to podcast list
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    JSONObject podcastObj = response.getJSONObject(i);
+                                    String title = podcastObj.getString("title");
+                                    String imageURL = podcastObj.getString("imageURL");
+                                    //búa til podcast hlut og setja í lista??
+                                    //Channel c = new Channel()
+                                    //channelList.add();
+                                } catch (JSONException e) {
+                                    Log.e(TAG, "JSON Parsing error: " + e.getMessage());
+                                }
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Server Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });*/
+    }
 }
