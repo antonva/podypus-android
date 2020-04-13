ed382e4c4c3ac6b24ce56351b91fd8387b1e9724
YlfaHa <ylh1@hi.is>
2020-04-13 20:49:09 +0000

    post for subscribe almost there

--- app/src/main/java/is/hi/hbv601g/podypus/ui/search/GridAdapter.java
+++ app/src/main/java/is/hi/hbv601g/podypus/ui/search/GridAdapter.java
@@ -1,8 +1,8 @@
 package is.hi.hbv601g.podypus.ui.search;
 
+import java.io.IOException;
 import java.util.List;
 
-import android.content.ClipData;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,11 +12,21 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import is.hi.hbv601g.podypus.R;
 import is.hi.hbv601g.podypus.entities.SearchItem;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class GridAdapter extends RecyclerView.Adapter<GridAdapter.SearchViewHolder> {
 
@@ -54,15 +64,19 @@ public class GridAdapter extends RecyclerView.Adapter<GridAdapter.SearchViewHold
     }
 
     @Override
-    public void onBindViewHolder(@NonNull SearchViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull SearchViewHolder holder, final int position) {
         final SearchItem si = mDataset.get(position);
         holder.textView.setText(si.collectionName);
         holder.imageView.setImageBitmap(si.image);
         holder.subscribeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.println(Log.INFO, "Button", "Subscribe Button clicked " + si.collectionName);
-
+                Log.println(Log.INFO, "Button", "Subscribe Button clicked " + si.feedUrl);
+                try {
+                    postSubscription(si);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
@@ -70,5 +84,41 @@ public class GridAdapter extends RecyclerView.Adapter<GridAdapter.SearchViewHold
     @Override
     public int getItemCount() {
         return mDataset.size();
+    }
+
+    //function that posts user's new subscriptions to server
+    private void postSubscription(SearchItem i) throws IOException {
+        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        String url = "https://podypus.punk.is/subscribe";
+        OkHttpClient client = new OkHttpClient();
+
+        JSONObject jo = new JSONObject();
+        final String reqBody = jo.toString();
+
+        try {
+            jo.put("username", "demouser");
+            jo.put("url", i.feedUrl);
+        } catch(JSONException e){
+            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(reqBody, JSON);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                String mMessage = response.body().string();
+                Log.println(Log.INFO, "post", mMessage);
+            }
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                String mMessage = e.getMessage().toString();
+                Log.println(Log.ERROR, "failure Response", mMessage);
+            }
+        });
     }
 }
